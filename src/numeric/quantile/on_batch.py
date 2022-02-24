5d99c5f4eaa6b34cbf4b90eac841d70bb2ebe29e
Wesley Runnels <wrunnels@mit.edu>
2022-02-24 11:06:29 -0500

    fixed quantile operator

--- src/numeric/quantile/on_batch.py
+++ src/numeric/quantile/on_batch.py
@@ -1,2 +1,1 @@
-new_col = pd.qcut(df[attributes['column'][0]], params['# quantiles'], labels=False)
-df[attributes['column'][0] + "_quantile"] = [x + 1 for x in new_col]
+df[attributes['column'][0] + "_quantile"] = pd.qcut(df[attributes['column'][0]], params['# quantiles'], duplicates='drop', labels=False)
