4da55f5c99760f1e14d340a18272e1377eda94fa
Wesley Runnels <wrunnels@mit.edu>
2022-06-07 17:28:46 +0100

    Remove rename UDO

--- src/dataframe processing/rename/on_batch.py
+++
@@ -1,6 +0,0 @@
-previous = [pair["Key"] for pair in params['columns to rename']]
-new = [pair["Value"] for pair in params['columns to rename']]
-
-d = dict(zip(previous, new))
-
-yield df.rename(d, axis=1)
