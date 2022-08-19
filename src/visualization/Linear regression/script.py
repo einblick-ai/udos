4648e6e9eb05e3be37a10d8f01771c0837f538e1
Wesley Runnels <wrunnels@mit.edu>
2022-08-19 18:38:39 -0400

    Update linear regression operator

--- src/visualization/Linear regression/script.py
+++ src/visualization/Linear regression/script.py
@@ -1,1 +1,15 @@
-df
+import pandas as pd
+import statsmodels.api as sm
+
+X = df[attributes['features']].drop(attributes['target'], axis=1, errors="ignore")
+if params['fit intercept']:
+    X = sm.add_constant(X, prepend=False)
+
+Y = df[attributes['target']]
+
+mod = sm.OLS(Y, X)
+res = mod.fit()
+
+result = pd.concat([res.params, res.pvalues], axis=1).transpose()
+result = pd.concat([pd.DataFrame({'': ['coefficient', 'p-value']}), result], axis=1)
+result
