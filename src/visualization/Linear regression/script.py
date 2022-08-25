c74bb4a4550f45a927da83991047b30d64c2c885
Wesley Runnels <wrunnels@mit.edu>
2022-08-25 17:25:43 -0400

    Add executor for linear regression

--- src/visualization/Linear regression/script.py
+++ src/visualization/Linear regression/script.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import statsmodels.api as sm
 
+dfs = []
+
+for df in self.from_input(0).flatten():
+    dfs.append(df)
+
+df = pd.concat(dfs, axis=0)
+print(df)
+
 X = df[attributes['features']].drop(attributes['target'], axis=1, errors="ignore")
 if params['fit intercept']:
     X = sm.add_constant(X, prepend=False)
 
 Y = df[attributes['target']]
 
-mod = sm.OLS(Y, X)
-res = mod.fit()
+model = sm.OLS(Y, X)
+fit = model.fit()
 
-result = pd.concat([res.params, res.pvalues], axis=1).transpose()
+self.save_state(fit)
+
+result = pd.concat([fit.params, fit.pvalues], axis=1).transpose()
 result = pd.concat([pd.DataFrame({'': ['coefficient', 'p-value']}), result], axis=1)
-result
+yield result
