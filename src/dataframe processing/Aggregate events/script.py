bd31d47cf308102ad36d7ec60108195282b54bae
Wesley Runnels <wrunnels@mit.edu>
2022-10-21 11:41:30 -0400

    Fix Aggregate events

--- src/dataframe processing/Aggregate events/script.py
+++ src/dataframe processing/Aggregate events/script.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # examples of windows: ["30min", "1h", "2h", "3h", "6h", "1d", "2d"]
-windows = params["Intervals"].split(";")
-date = attributes['Date'][0]
-group = attributes['Group']
-closed = params['Closed'][0]
-columns = attributes['Columns']
-aggregation_function = params['Aggregation function'][0]
+windows = params["intervals"].split(";")
+date = attributes['date'][0]
+group = attributes['group']
+closed = params['closed'][0]
+columns = attributes['columns']
+aggregation_function = params['aggregation function'][0]
 
 df = df.sort_values(by=[date] + group)
 df[date] = pd.to_datetime(df[date], unit="ms")
@@ -14,15 +14,15 @@ df.index = df[date]
 
 for window in windows:
     groupby = df.groupby(by=group, sort=False).rolling(window, closed=closed)[columns]
-    if aggregation_function == "Sum":
+    if aggregation_function == "sum":
         roll = groupby.sum()
-    elif aggregation_function == "Mean":
+    elif aggregation_function == "mean":
         roll = groupby.mean()
-    elif aggregation_function == "Median":
+    elif aggregation_function == "median":
         roll = groupby.median()
-    elif aggregation_function == "Min":
+    elif aggregation_function == "min":
         roll = groupby.min()
-    elif aggregation_function == "Max":
+    elif aggregation_function == "max":
         roll = groupby.max()
     roll = roll.sort_values(by=[date] + group)
     roll.columns += "_by_" +"_".join(group) + "_in_" + window
