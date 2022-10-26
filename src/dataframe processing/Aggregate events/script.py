bfe5f6c79b96227ec7ce5a0eb32db1137a4abe7e
wrunn <111093706+wrunn@users.noreply.github.com>
2022-10-26 11:52:06 -0400

    NOR-6809: Capitalize all UDO inputs (#6)
    * Update Aggregate events to take in an aggregation function and capitals
    
    * Add keyword to remove duplicate rows
    
    * Update UDOs to have capitalized display names
    
    * Fix Aggregate events
    
    * Fix height of Top N rows
    
    Co-authored-by: Wesley Runnels <wrunnels@mit.edu>

--- src/dataframe processing/Aggregate events/script.py
+++ src/dataframe processing/Aggregate events/script.py
@@ -6,13 +6,24 @@ date = attributes['date'][0]
 group = attributes['group']
 closed = params['closed'][0]
 columns = attributes['columns']
+aggregation_function = params['aggregation function'][0]
 
 df = df.sort_values(by=[date] + group)
 df[date] = pd.to_datetime(df[date], unit="ms")
 df.index = df[date]
 
 for window in windows:
-    roll = df.groupby(by=group, sort=False).rolling(window, closed=closed)[columns].sum()
+    groupby = df.groupby(by=group, sort=False).rolling(window, closed=closed)[columns]
+    if aggregation_function == "sum":
+        roll = groupby.sum()
+    elif aggregation_function == "mean":
+        roll = groupby.mean()
+    elif aggregation_function == "median":
+        roll = groupby.median()
+    elif aggregation_function == "min":
+        roll = groupby.min()
+    elif aggregation_function == "max":
+        roll = groupby.max()
     roll = roll.sort_values(by=[date] + group)
     roll.columns += "_by_" +"_".join(group) + "_in_" + window
     df[roll.columns] = roll[roll.columns].values
