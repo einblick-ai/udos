5b0a242e81e898146abf3af4dce34a3b3ccff08a
wrunn <111093706+wrunn@users.noreply.github.com>
2022-12-21 12:16:44 -0500

    NOR-7316: Fill NaN values doubles input and doesn't respect axis parameter (#14)
    Fill NaN values doubles input and doesn't respect axis parameter
    
    Co-authored-by: Wesley Runnels <wrunnels@mit.edu>

--- src/dataframe processing/Remove NaN values/script.py
+++ src/dataframe processing/Remove NaN values/script.py
@@ -1,23 +1,9 @@
 import numpy as np
 
-# 0, or ‘index’ : Drop rows which contain missing values.
-# 1, or ‘columns’ : Drop columns which contain missing value
-
-self._axis = 1 if params["axis"] == "drop columns" else 0
-
-
-# set how field
-if params["min % valid values"] == 100:
-    self._how = "all"
-else:
-    self._how = "any"
-
-self._relative_threshold = float(params["min % valid values"]) / 100
-self._drop_nan = params["drop NaN"]
-self._drop_infinity = params["drop infinity"]
-self._first_batch = True
-self._columns_to_keep = None
-
+axis = 1 if params["axis"][0] == "drop columns" else 0
+relative_threshold = float(params["min % valid values"]) / 100
+drop_nan = params["drop NaN"]
+drop_infinity = params["drop infinity"]
 
 def get_absolute_threshold(batch):
     """
@@ -26,13 +12,10 @@ def get_absolute_threshold(batch):
     Args:
         batch (pd.DataFrame): batch from input stream
     """
-    if self._axis == 1:
-        dimension = len(batch)  # columns threshold
-    else:
-        dimension = len(batch.columns)  # rows threshold
-    self._absolute_threshold = int(dimension * self._relative_threshold)
+    dimension = len(batch) if axis == 1 else len(batch.columns)
+    return int(dimension * relative_threshold)
 
-def drop(batch: pd.DataFrame):
+def drop(batch: pd.DataFrame, abs_threshold: int):
     """
     Drop columns or rows based on mode and absolute-threshold
 
@@ -42,29 +25,17 @@ def drop(batch: pd.DataFrame):
     Returns:
         preprocessed_batch (pd.DataFrame): preprocessed batch
     """
-    # drop infinity and nan
-    if self._drop_infinity and self._drop_nan:
+    if drop_infinity and drop_nan:
         with pd.option_context("mode.use_inf_as_null", True):
-            preprocessed_batch = batch.dropna(axis=self._axis,
-                                                how=self._how,
-                                                thresh=self._absolute_threshold)
-    # drop just nans
-    elif self._drop_nan:
-        preprocessed_batch = batch.dropna(axis=self._axis,
-                                            how=self._how,
-                                            thresh=self._absolute_threshold)
-    # drop just infinity
-    elif self._drop_infinity:
+            return batch.dropna(axis=axis, thresh=abs_threshold)
+    elif drop_nan:
+        return batch.dropna(axis=axis, thresh=abs_threshold)
+    elif drop_infinity:
         mask = (batch == np.infty) | (batch == -np.infty)
-        keep = (mask.sum(axis=self._axis) <= self._absolute_threshold).values
-        if self._axis == 1:
-            preprocessed_batch = batch[keep]
-        else:
-            preprocessed_batch = batch.loc[keep, :]
-    # dummy drop (no effect)
+        keep = (mask.sum(axis=axis) <= abs_threshold).values
+        return batch[keep] if axis == 1 else batch.loc[keep, :]
     else:
-        preprocessed_batch = batch
-    return preprocessed_batch
+        return batch
 
 """
 Drop columns/rows for the input stream based on the first batch
@@ -78,27 +49,20 @@ Args:
 Returns:
     pd.DataFrame: preprocessed batch
 """
+first_batch = True
+columns_to_keep = None
+absolute_threshold = None
 
-ds = self.from_input(0)
+for dfs in self.from_input(0):
+    for batch in dfs:
+        if first_batch:
+            absolute_threshold = get_absolute_threshold(batch)
+            preprocessed_batch = drop(batch, absolute_threshold)
 
-for dfs in ds:
-    for df in dfs:
-        batch = df
-        if self._first_batch:
-            get_absolute_threshold(batch)
-            preprocessed_batch = drop(batch)
-
-            # for columns save dropped columns and apply same rule on next batches
-            if self._axis == 1:
-                self._columns_to_keep = preprocessed_batch.columns
-            self._first_batch = False
+            # for columns, save dropped columns and apply the same rule on future batches
+            if axis == 1:
+                columns_to_keep = preprocessed_batch.columns
+            first_batch = False
             yield preprocessed_batch
-
-        # drop columns based on first batch
-        if self._axis == 1:
-            preprocessed_batch = batch[self._columns_to_keep]
-        # drop rows
         else:
-            preprocessed_batch = drop(batch)
-
-        yield preprocessed_batch
+            yield batch[columns_to_keep] if axis == 1 else drop(batch, absolute_threshold)
